ac9d605522ab7b01695e203e5e6adcf5ebd9492f
EduardoMVBit <especialista.react@bit.institute>
2023-04-21 11:23:17 -0300

    Add All Providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { UsersProvider } from "./context/Users";
+import { UserProvider } from "./context/User";
+import { ProductsProvider } from "./context/Products";
+import { SearchProvider } from "./context/Search";
+import { ProductProvider } from "./context/Product";
+import { CartProvider } from "./context/Cart";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <UsersProvider>
+      <UserProvider>
+        <ProductsProvider>
+          <SearchProvider>
+            <ProductProvider>
+              <CartProvider>
+                <App />
+              </CartProvider>
+            </ProductProvider>
+          </SearchProvider>
+        </ProductsProvider>
+      </UserProvider>
+    </UsersProvider>
   </React.StrictMode>
 );
