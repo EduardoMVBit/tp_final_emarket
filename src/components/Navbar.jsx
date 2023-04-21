b8ce351b26e8a2583bb7ea7791e6dd36aa5b97ac
EduardoMVBit <especialista.react@bit.institute>
2023-04-21 00:48:56 -0300

    Prepare Start Components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = function () {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
---
+++ src/components/Search.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search = function () {
+  return <div>Search</div>;
+};
+
+export default Search;
