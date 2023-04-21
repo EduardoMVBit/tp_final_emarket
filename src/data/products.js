e445e5aa0480081de22328b612e7d5e6bd9a9431
EduardoMVBit <especialista.react@bit.institute>
2023-04-21 10:33:56 -0300

    Add Default Data

---
+++ src/data/products.js
@@ -0,0 +1,42 @@
+let catalogoData = [];
+catalogoData.push({
+  id: 1,
+  name: "milanesa napolitana",
+  precio: 10,
+  descripcion: "Milanesa con queso muzzarela y tomate",
+  imagen:
+    "https://www.paulinacocina.net/wp-content/uploads/2015/03/P1150541-e1439164269502.jpg",
+  categoria: "Carnes",
+});
+catalogoData.push({
+  id: 2,
+  name: "Estofado de ternera",
+  precio: 50,
+  descripcion:
+    "Bienvenidos amigas y amigos de Paulina Cocina! En el capítulo de hoy vamos a ver cómo hacer un buen estofado de carne, y no es uno cualquiera, es un estofado de carne de ternera que se deshace en la boca.",
+  imagen:
+    "https://www.paulinacocina.net/wp-content/uploads/2020/05/goulash-1696582-1280.jpg",
+  categoria: "Carnes",
+});
+catalogoData.push({
+  id: 3,
+  name: "solomillo Wellington de cerdo con un relleno de champignones",
+  precio: 10,
+  descripcion:
+    "Aprendé esta receta de solomillo Wellington de cerdo con un relleno de champignones envuelto en una capa crujiente de hojaldre: tradición y sofisticación en una misma preparación ¡No te la pierdas!",
+  imagen:
+    "https://www.paulinacocina.net/wp-content/uploads/2023/01/solomillo-wellington-800x450.jpg",
+  categoria: "Carnes",
+});
+catalogoData.push({
+  id: 4,
+  name: "Chiles en nogada",
+  precio: 20,
+  descripcion:
+    "Los chiles en nogada son uno de los platillos más representativos de México. Esta deliciosa receta combina los sabores dulces y salados en una mezcla única. Con nuestra receta tradicional y variaciones, podrás preparar este platillo auténtico en casa y sorprender a tus invitados con una explosión de sabores.",
+  imagen:
+    "https://www.paulinacocina.net/wp-content/uploads/2023/03/chile-en-nogada-800x450.jpg",
+  categoria: "Vegetariano",
+});
+
+export default catalogoData;
---
+++ src/data/users.js
@@ -0,0 +1,8 @@
+let userData = [];
+userData.push({ username: "eduardo", password: "eduardo", roll: "cliente" });
+userData.push({
+  username: "administrador",
+  password: "admin",
+  roll: "administrador",
+});
+export default userData;
